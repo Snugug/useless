ed65e64e05a26690a97b82a4465a12b77bbd2bf7
Sam Richard <snugug@gmail.com>
2014-10-05 10:51:11 -0400

    um, done?

--- js/useless.js
+++ js/useless.js
@@ -10,18 +10,16 @@
 
     var getRandomInt = function (min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
-    }
+    };
 
     var loading = function () {
       value += getRandomInt(1, 20);
-      console.log(value);
       progress.setAttribute('value', value);
 
       if (value >= max) {
         clearInterval(animate);
-        console.log(window.history);
         if (window.history.length === 1) {
-          window.location.href = 'https://github.com/snugug/useless'
+          window.location.href = 'https://github.com/snugug/useless';
         }
         else {
           window.history.forward();
